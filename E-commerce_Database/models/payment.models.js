a2fb08957d042363f53169188aab784b60bb2cd9
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:28:30 +0700

    Auto commit

---
+++ E-commerce_Database/models/payment.models.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const paymentMethodSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["card", "bank", "wallet"],
+    required: true,
+  },
+
+  provider: {
+    type: String,
+    enum: ["stripe", "paypal", "braintree"],
+    required: true,
+  },
+
+  providerPaymentMethodId: {
+    type: String,
+    required: true,
+  },
+
+  brand: String,      // Visa, Mastercard
+  last4: String,      // 4242
+  expMonth: Number,
+  expYear: Number,
+
+  isDefault: {
+    type: Boolean,
+    default: false,
+  },
+}, { timestamps: true });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+
+  paymentMethods: [paymentMethodSchema],
+});
+
+module.exports = mongoose.model("User", userSchema);

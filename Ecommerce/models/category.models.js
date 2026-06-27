d2db1a8f4d0beaf91ec56e844a60bdfcd0197db1
Mudasir <apple.mudasir@gmail.com>
2026-06-27 16:05:18 +0700

    Auto commit

--- Ecommerce/models/category.models.js
+++ Ecommerce/models/category.models.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-    
+    name
 },{timestamps:true}) 
 
 export const Category = mongoose.model("category",categorySchema)

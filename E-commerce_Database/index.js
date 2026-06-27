1d995c8edfb942c4f8116d085d9c13ccdf9239bd
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:05:50 +0700

    Auto commit

--- E-commerce_Database/index.js
+++ E-commerce_Database/index.js
@@ -14,7 +14,7 @@ const SupplierSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    contactti:{
+    contacttitle:{
         type: String,
         required: true,
     }

6cc3be5140225a7ed753f55e9710f11e5df7d98d
Mudasir <apple.mudasir@gmail.com>
2026-06-27 15:37:18 +0700

    Auto commit

--- models/todo.models.js
+++ models/todo.models.js
@@ -1,7 +1,27 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-    
+    content:{
+        type: String,
+        lowercase: true,
+        required: true
+    },
+    complete:{
+        type: Boolean,
+        default: false
+    }, 
+
+    createdBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+
+    subtodos:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ] // Array of Sub Todos 
 
 },{timestamps:true}) 
 export const User = mongoose.model("User",TodoSchema);

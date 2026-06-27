fe284b87e088c1b15390964033125c6298629be5
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:31:39 +0700

    Auto commit

--- E-commerce_Database/models/payment.models.js
+++ E-commerce_Database/models/payment.models.js
@@ -29,4 +29,4 @@ const paymentMethodSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-export const Payment = mongoose.model("Payment",)
+export const Payment = mongoose.model("Payment",pa)

6eecda87aa5f58d69a5c54704b78923ada136ce5
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:42:37 +0700

    Auto commit

--- E-commerce_Database/models/shippers.models.js
+++ E-commerce_Database/models/shippers.models.js
@@ -5,7 +5,7 @@ const ShippingScheme = new mongoose.Schema({
         type: String,
         lowercase: true,
         unique: true,
-        required
+        required: tre
     }
 },{timestamps:true})
 

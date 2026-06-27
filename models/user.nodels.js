78b952aa3522eb2392cb1326708d6b978ff22c7a
Mudasir <apple.mudasir@gmail.com>
2026-06-27 15:12:23 +0700

    Auto commit

--- models/user.nodels.js
+++ models/user.nodels.js
@@ -1,13 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema= new mongoose.Schema({
-
-    // one way to us modeling 
-
-    username:String,
-    email:String,
-    password:String,
-    isAtive: Boolean
+    user
 
 },{timestamps:true})
 

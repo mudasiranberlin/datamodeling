c2ab4858b9a00fb29b47bc95b70583cd39691fd8
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:09:20 +0700

    Auto commit

--- E-commerce_Database/models/address.model.js
+++ E-commerce_Database/models/address.model.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({},{});
 
-export const address = mongoose.model("address",) 
+export const address = mongoose.model("address",addressSchema) 

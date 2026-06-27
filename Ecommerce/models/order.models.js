1b7982719ecaf508648ddbf826b43561e231468f
Mudasir <apple.mudasir@gmail.com>
2026-06-27 16:31:41 +0700

    Auto commit

--- Ecommerce/models/order.models.js
+++ Ecommerce/models/order.models.js
@@ -25,20 +25,20 @@ const OrderSchema = new mongoose.Schema({
     orderitems:{
         type:[orderItemsSchema]. // this one is second way u can see top i have define it.
 
-        or you can also write like this previously use it 
-        both are valid
+    //     or you can also write like this previously use it 
+    //     both are valid
 
-        type:[
-        {
-           productId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "product"
-    },
-    quantity:{
-        type: Number,
-        required: true
-    } 
-        }]
+    //     type:[
+    //     {
+    //        productId:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "product"
+    // },
+    // quantity:{
+    //     type: Number,
+    //     required: true
+    // } 
+    //     }]
 
         // new concept u have to undertand : 3:/// 32:// 45
     }

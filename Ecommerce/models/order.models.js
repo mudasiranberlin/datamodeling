c195e1a0ffc7fc4f8d602b8015dbe3f9d9efbd2e
Mudasir <apple.mudasir@gmail.com>
2026-06-27 16:22:04 +0700

    Auto commit

--- Ecommerce/models/order.models.js
+++ Ecommerce/models/order.models.js
@@ -2,9 +2,5 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema({},{timestamps})
 
-OrderPrice:{
-    type: String
-}
-
 
 export const Order = mongoose.model("Order",OrderSchema)

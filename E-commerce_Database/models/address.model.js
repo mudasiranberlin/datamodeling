3e60e97b44e9294f75a02240cb6a854c8e8ad655
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:16:13 +0700

    Auto commit

--- E-commerce_Database/models/address.model.js
+++ E-commerce_Database/models/address.model.js
@@ -26,7 +26,7 @@ const addressSchema = new mongoose.Schema({
 
     country:{
         type: String,
-        enum: ["germany","","Delhi"],
+        enum: ["germany","india","Delhi"],
         default: "jammu"
     }
 

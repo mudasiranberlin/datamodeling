d1a0bfd4e97d39afe4cfea8e549ded23da0714b5
Mudasir <apple.mudasir@gmail.com>
2026-06-27 17:10:03 +0700

    Auto commit

---
+++ E-commerce_Database/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const SupplierSchema = new mongoose.Schema({
+    companyname:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    contactfname:{
+        type: String,
+        required: true,
+    },
+    contactlname:{
+        type: String,
+        required: true,
+    },
+    contacttitle:{
+        type: String,
+        required: true,
+    },
+
+    address:{
+        type:[addressscheme]
+    }
+
+},{timestamps:true});
+
+export const Supplier = mongoose.model("Supplier",SupplierSchema)

d873f9c9bc58cbf4c1e0aeb7c68e10d19fa311f2
Mudasir <apple.mudasir@gmail.com>
2026-06-27 16:45:17 +0700

    Auto commit

--- Ecommerce/models/order.models.js
+++ Ecommerce/models/order.models.js
@@ -9,7 +9,9 @@ const adressSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    streetaddress
+    streetaddress:{
+        
+    }
 })
 
  /// New concept here 

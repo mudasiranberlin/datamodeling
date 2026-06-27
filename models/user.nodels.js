4c0631156860a572ac1c73e140f0d6302c6c5306
Mudasir <apple.mudasir@gmail.com>
2026-06-27 15:14:30 +0700

    Auto commit

--- models/user.nodels.js
+++ models/user.nodels.js
@@ -8,7 +8,9 @@ const UserSchema= new mongoose.Schema({
         required: true
     },
     password:{
-        type: String
+        type: String,
+        required: true,
+        
     }
 
 },{timestamps:true})

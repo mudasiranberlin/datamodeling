ba00d0b0c9f53a28867ad7aea9f3004576572694
Mudasir <apple.mudasir@gmail.com>
2026-06-27 15:12:13 +0700

    Auto commit

---
+++ models/2nd_way_user.models.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema= new mongoose.Schema({
+
+    // one way to us modeling 
+
+    username:String,
+    email:String,
+    password:String,
+    isAtive: Boolean
+
+},{timestamps:true})
+
+const User = mongoose.model("User",UserSchema)

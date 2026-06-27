f96ecb628d60ed80e2da22778f05203ec62cc9b3
Mudasir <apple.mudasir@gmail.com>
2026-06-27 16:14:31 +0700

    Auto commit

--- Ecommerce/models/product.models.js
+++ Ecommerce/models/product.models.js
@@ -28,7 +28,8 @@ const productSchema= new mongoose.Schema({
     },
     owner:{
         type: mongoose.Schema.Types.ObjectId,
-        ref:"User"
+        ref:"User",
+        req
     }
 
 },{timestamps:true})

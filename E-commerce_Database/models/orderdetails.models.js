51ddefefc2433a7ac76be7f05cbc375f8daa8f81
Mudasir <apple.mudasir@gmail.com>
2026-06-28 10:53:16 +0700

    Auto commit

--- E-commerce_Database/models/orderdetails.models.js
+++ E-commerce_Database/models/orderdetails.models.js
@@ -37,7 +37,7 @@ const OrderdetailsSchema = new mongoose.Schema({
     },
     fulfilled:{
         type : String,
-        enum : ["AMAZON","FLIPKART","MEESHO"]
+        enum : ["AMAZON","FLIPKART","MEESHO"],
     }
 
 },{timestamps:true})

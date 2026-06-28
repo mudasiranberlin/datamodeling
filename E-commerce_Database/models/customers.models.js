746f5d68b3b39292e9d8a79f660f0963e5d573ec
Mudasir <apple.mudasir@gmail.com>
2026-06-28 11:10:07 +0700

    Auto commit

--- E-commerce_Database/models/customers.models.js
+++ E-commerce_Database/models/customers.models.js
@@ -4,4 +4,4 @@ const customerSchema = new mongoose.Schema({
 
 },{timestamps:true})
 
-export const Customer = mongoose.model("Customer",customerSchema)
+export const Customer = mongoose.model("Customer",customerSchema);

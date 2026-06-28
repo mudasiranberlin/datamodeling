ab3993ffe21e90cf35a2ac5d1f8217fcb9fcb829
Mudasir <apple.mudasir@gmail.com>
2026-06-28 11:13:48 +0700

    Auto commit

--- E-commerce_Database/models/customers.models.js
+++ E-commerce_Database/models/customers.models.js
@@ -19,7 +19,7 @@ const customerSchema = new mongoose.Schema({
         required: true,
     },
     address:{
-        type: mongoose.Schema.Types
+        type: mongoose.Schema.Types.
     }
 
 },{timestamps:true})

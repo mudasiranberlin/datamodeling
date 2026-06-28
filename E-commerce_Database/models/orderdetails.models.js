42da541ab960fbda4dabc106e16b8e114d4c59d7
Mudasir <apple.mudasir@gmail.com>
2026-06-28 10:50:57 +0700

    Auto commit

--- E-commerce_Database/models/orderdetails.models.js
+++ E-commerce_Database/models/orderdetails.models.js
@@ -28,7 +28,7 @@ const OrderdetailsSchema = new mongoose.Schema({
     size:{
         type: String,
         enum: ["XXL","XL","X","L","M","S"]
-        
+        req
     }
 
 },{timestamps:true})
